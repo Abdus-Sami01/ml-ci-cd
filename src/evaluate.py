74a0ab892bbcce21c485773dadf8bc941f6ed8fe
Abdus-Sami01 <muhammadabdulsami7@gmail.com>
2025-12-24 21:55:33 +0500

    minor chnanging in evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -7,8 +7,8 @@ def evaluate():
     df = pd.read_csv(r"D:/python files/MLops/ml-ci-cd/data/raw.csv")
     model = joblib.load(r"models/model.pkl")
 
-    X = df.drop("price", axis=1)
-    y = df["price"]
+    X = df.drop("condition", axis=1)
+    y = df["condition"]
 
     score = r2_score(y, model.predict(X))
     mlflow.log_metric("r2", score)

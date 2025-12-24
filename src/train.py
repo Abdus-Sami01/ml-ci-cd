76e7ac414902ff3b6168e0c0d869da009ed825e9
Abdus-Sami01 <muhammadabdulsami7@gmail.com>
2025-12-24 21:45:40 +0500

    initial training model strategy

---
+++ src/train.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import yaml
+import mlflow
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+def train():
+    params = yaml.safe_load(open("params.yaml"))
+    df = pd.read_csv("data/raw_processed.csv")
+
+    X = df.drop("condition", axis=1)
+    y = df["condition"]
+
+    mlflow.start_run()
+    model = RandomForestRegressor(
+        n_estimators=params["train"]["n_estimators"]
+    )
+    model.fit(X, y)
+
+    mlflow.log_params(params["train"])
+    mlflow.sklearn.log_model(model, "model")
+
+    joblib.dump(model, "models/model.pkl")
+    mlflow.end_run()

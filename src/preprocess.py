5f2beff36f63193d8cc9da2bbdf44e721466fe17
Abdus-Sami01 <muhammadabdulsami7@gmail.com>
2025-12-24 21:36:59 +0500

    initial training strategy

---
+++ src/preprocess.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+input_path="D:\python files\MLops\ml-ci-cd\data\raw.csv"
+output_path="D:\python files\MLops\ml-ci-cd\data\raw_processed.csv"
+
+
+def preprocess(input_path, output_path):
+    df = pd.read_csv(input_path)
+    df = df.dropna()
+    df=df.drop_duplicates()
+    df.to_csv(output_path, index=False)
